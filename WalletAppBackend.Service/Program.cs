3cc60265578b665288e27325ef3dcf42f30e1842
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-19 19:32:36 +0300

    Add project files.

---
+++ WalletAppBackend.Service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletAppBackend.Service
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
