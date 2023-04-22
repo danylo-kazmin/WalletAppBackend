902258cc8b4aa0c4fa37a603e1b44f5b9e528020
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-22 19:55:18 +0300

    Added Entities

---
+++ WalletAppBackend.Infrastructure/DataAccess/Implementation/Entities/CardBalanceEntity.cs
@@ -0,0 +1,14 @@
+﻿
+using WalletAppBackend.Infrastructure.DataAccess.Contracts;
+
+namespace WalletAppBackend.Infrastructure.DataAccess.Implementation.Entities
+{
+    public class CardBalanceEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public decimal MaxLimit { get; set; }
+        public decimal Balance { get; set; }
+        public Guid UserId { get; set; }
+        public UserEntity User { get; set; }
+    }
+}
---
+++ WalletAppBackend.Infrastructure/DataAccess/Implementation/Entities/UserEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletAppBackend.Infrastructure.DataAccess.Contracts;
+
+namespace WalletAppBackend.Infrastructure.DataAccess.Implementation.Entities
+{
+    public class UserEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public List<TransactionEntity> Transactions { get; set; }
+        public DailyPointsEntity DailyPoints { get; set; }
+        public CardBalanceEntity CardBalance { get; set; }
+    }
+}
