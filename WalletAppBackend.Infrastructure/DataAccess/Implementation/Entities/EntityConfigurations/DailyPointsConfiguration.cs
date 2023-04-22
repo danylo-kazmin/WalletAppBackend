f889c08dd987ed0e1ff764261ab3a9d07d580459
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-22 19:55:05 +0300

    Added Entity Configurations

---
+++ WalletAppBackend.Infrastructure/DataAccess/Implementation/Entities/EntityConfigurations/DailyPointsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WalletAppBackend.Infrastructure.DataAccess.Implementation.Entities;
+
+namespace WalletAppBackend.Infrastructure.DataAccess.Implementation.EntityConfigurations
+{
+    public class DailyPointsConfiguration : IEntityTypeConfiguration<DailyPointsEntity>
+    {
+        public void Configure(EntityTypeBuilder<DailyPointsEntity> builder)
+        {
+            builder.ToTable("DailyPoints").HasKey(p => p.Id);
+            builder.Property(p => p.Points).IsRequired().HasColumnName("Points").HasColumnType("int");
+
+            builder.HasOne(d => d.User)
+                .WithOne(u => u.DailyPoints)
+                .HasForeignKey<DailyPointsEntity>(d => d.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
