9a938997d21f2167bdb01bee464e1ed3c1b90c7c
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-23 19:15:49 +0300

    Fixed models

--- WalletAppBackend.Service/Models/Transaction.cs
+++ WalletAppBackend.Service/Models/Transaction.cs
@@ -11,7 +11,7 @@ namespace WalletAppBackend.Service.Models
         public Guid Id { get; set; }
         public string Type { get; set; }
         public decimal Amount { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
         public string Description { get; set; }
         public DateTimeOffset Date { get; set; }
         public string Status { get; set; }
