01e228953aa2deb2162c0eb2a04f5b03de43bf50
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-23 16:53:38 +0300

    Added helpers

---
+++ WalletAppBackend.API/Helpers/KeyNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace WalletAppBackend.API.Helpers
+{
+    public class KeyNotFoundException : Exception
+    {
+        public KeyNotFoundException()
+            : base() { }
+
+        public KeyNotFoundException(string message)
+            : base(message) { }
+
+        public KeyNotFoundException(string message, params object[] args)
+            : base(String.Format(CultureInfo.CurrentCulture, message, args)) { }
+    }
+}
