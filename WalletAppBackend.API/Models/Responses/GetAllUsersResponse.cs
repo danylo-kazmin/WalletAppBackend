5dff12890fb463c3e99b751049e1fa2f9d3be5c7
Anton Kravchenko <anton.kravchenko.ext@bedegaming.com>
2023-04-23 19:17:49 +0300

    Added responses

---
+++ WalletAppBackend.API/Models/Responses/GetAllUsersResponse.cs
@@ -0,0 +1,9 @@
+﻿using WalletAppBackend.Service.Models;
+
+namespace WalletAppBackend.API.Models.Responses
+{
+    public class GetAllUsersResponse
+    {
+        public List<User> Users { get; set; }
+    }
+}
---
+++ WalletAppBackend.API/Models/Responses/GetDailyPointsResponse.cs
@@ -0,0 +1,9 @@
+﻿using WalletAppBackend.Service.Models;
+
+namespace WalletAppBackend.API.Models.Responses
+{
+    public class GetDailyPointsResponse
+    {
+        public DailyPoints DailyPoints { get; set; }
+    }
+}
